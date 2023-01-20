800e19395bb15308257b606df082a1a9dc5da43e
Joshua Goldschmidt <jos5798s@hs-coburg.de>
2023-01-20 23:56:53 +0100

    Kapitel 3 nur änderungen an Building Klasse Ich habe mich für die Idee von Carla entschieden, da sie meiner Meinung nach eine klarere und bessere Struktur in die Materialverwaltung bringt.

--- Capycity/Building.cpp
+++ Capycity/Building.cpp
@@ -2,7 +2,17 @@
 #include <vector>
 #include <iostream>
 #include "Material.h"
+#include "Wood.h"
+#include "Metal.h"
+#include "Plastic.h"
+#include <map>
+#include <vector>
 using std::string;
+using std::map;
+using std::inserter;
+using std::pair;
+
+
 
 Building::Building(double _Grundpreis, string _Label, int _id, string ma1, string ma2, string ma3) {
 	Grundpreis = _Grundpreis;
@@ -11,6 +21,7 @@ Building::Building(double _Grundpreis, string _Label, int _id, string ma1, strin
 	setBauMateriallien(ma1);
 	setBauMateriallien(ma2);
 	setBauMateriallien(ma3);
+
 }
 
 void Building::setBauMateriallien(string _bauMateriallien) {
@@ -18,9 +29,21 @@ void Building::setBauMateriallien(string _bauMateriallien) {
 }
 
 string Building::getBauMateriallien(int _value) {
-	
 	return bauMateriallien.at(_value);
 }
+
+void Building::setBauMateriallienWood(string _bauMateriallienWood) {
+	bauMateriallienWood.push_back(_bauMateriallienWood);
+}
+
+void Building::setBauMateriallienMetal(string _bauMateriallienMetal) {
+	bauMateriallienMetal.push_back(_bauMateriallienMetal);
+}
+
+void Building::setBauMateriallienPlastic(string _bauMateriallienPlastic) {
+	bauMateriallienPlastic.push_back(_bauMateriallienPlastic);
+}
+
 
 double Building::getGrundpreis() {
 	return Grundpreis;
@@ -31,4 +54,24 @@ string Building::getLabelById(int _id) {
 		return Label;
 	}
 	return "";
+}
+
+void Building::getAmoutOfMaterialNeeded(string _name) {
+	/*
+	map<Material, vector<string>> mapGebaeude;
+	mapGebaeude.insert(pair<Material, vector<string>> (wo2, bauMateriallienWood));
+	mapGebaeude.insert(pair<Material, vector<string>> (me2, bauMateriallienMetal));
+	mapGebaeude.insert(pair<Material, vector<string>> (pa2, bauMateriallienPlastic));
+	*/
+
+	setBauMateriallienMetal("Metal");
+	setBauMateriallienPlastic("Plastik");
+	setBauMateriallienWood("Wood");
+
+		map<Material, vector<string>> mapGebaeude;
+	mapGebaeude[Wood("Holz", 10.5, 1)] = bauMateriallienWood;
+	mapGebaeude[Metal("Metall", 20.2, 1)] = bauMateriallienMetal;
+	mapGebaeude[Plastic("Plastik", 5.1, 1)] = bauMateriallienPlastic;
+	
+
 }
--- Capycity/Building.h
+++ Capycity/Building.h
@@ -3,6 +3,7 @@
 #pragma once
 #include <iostream>
 #include <vector>
+#include <map>
 
 using std::string;
 using std::vector;
@@ -14,11 +15,19 @@ private:
 	string Label;
 	vector <string> bauMateriallien;
 	int id;
+	vector <string> bauMateriallienWood;
+	vector <string> bauMateriallienMetal;
+	vector <string> bauMateriallienPlastic;
+
 public:
 	Building(double _Grundpreis, string _Label,int _id, string ma1, string ma2, string ma3);
 	virtual void setBauMateriallien(string _bauMateriallien);
 	virtual string getBauMateriallien(int _value);
 	virtual double getGrundpreis();
 	virtual string getLabelById(int _id);
+	virtual void getAmoutOfMaterialNeeded(string _name);
+	virtual void setBauMateriallienWood(string _bauMateriallienWood);
+	virtual void setBauMateriallienPlastic(string _bauMateriallienPlastic);
+	virtual void setBauMateriallienMetal(string _bauMateriallienMetal);
 };
 #endif
